8a197bfc9dcb245e63174eb8b93de3c08ec8cf00
CristianEscequiel <esce.arguello21@gmail.com>
2026-06-29 11:04:26 -0300

    feat: add booking detail and selection flow

---
+++ src/app/features/bookings/components/class-detail/class-detail.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+import { Booking } from '../../../../models/booking.model';
+
+@Component({
+  selector: 'app-class-detail',
+  imports: [],
+  templateUrl: './class-detail.html',
+  styleUrl: './class-detail.scss',
+})
+export class ClassDetail {
+
+  @Input() classItem : Booking | null = null;
+
+
+}
--- src/app/features/bookings/components/class-list/class-list.ts
+++ src/app/features/bookings/components/class-list/class-list.ts
@@ -1,9 +1,10 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { BookingService } from '../../../../service/booking.service';
 import { Booking } from '../../../../models/booking.model';
+import { ClassDetail } from '../class-detail/class-detail';
 @Component({
   selector: 'app-class-list',
-  imports: [],
+  imports: [ClassDetail],
   templateUrl: './class-list.html',
   styleUrl: './class-list.scss',
 })
@@ -11,11 +12,18 @@ export class ClassList implements OnInit {
 
 bookingService = inject(BookingService);
 $classList = signal<Booking[]>([]);
+$classSelected = signal<Booking | null>(null);
 
 ngOnInit() {
   this.bookingService.getBookings().subscribe(bookings => {
     console.log('Bookings:', bookings);
     this.$classList.set(bookings);
   });
+}
+showClassDetails(classId: number): void {
+  const selectedClass = this.$classList().find(
+    classItem => classItem.id === classId
+  );
+  this.$classSelected.set(selectedClass ?? null);
 }
 }
